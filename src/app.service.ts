8046bd2e1617ea05bc1f4afc2e598720e68d585b
Samandar <iwtuz166@gmail.com>
2024-02-04 14:11:06 +0500

    add comments

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): object {
-    return { message: 'Server is run' };
+    return { message: 'Server is run on port 5001' };
   }
 }
